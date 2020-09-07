039ae693bbd5418c8a0e31940b1100a9216af98a
Kyle Gohr <kyle.279965@nv.ccsd.net>
2020-09-07 02:31:46 +0000

    I did stuff.

--- src/main/java/SecondsPastMidnight.java
+++ src/main/java/SecondsPastMidnight.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
-
+ 
 class SecondsPastMidnight {
-    public static void main(String[] args) {
-
-        //Creates a new scanner for user input
-        Scanner scanner = new Scanner(System.in);
-
-        //Reads int from user
-        int number = scanner.nextInt();
-
-        //Your code goes here
-
-
-        // closing the scanner object
-        scanner.close();
-    }
+   public static void main(String[] args) {
+ 
+       //Creates a new scanner for user input
+       Scanner scanner = new Scanner(System.in);
+ 
+       //Reads int from user
+       int seconds = scanner.nextInt();
+       int hours;
+       int mins;
+ 
+       //Your code goes here
+     hours = seconds / 3600;
+     mins = seconds / 60;
+     seconds = seconds / 1;
+     System.out.println(hours + " " + mins);
+ 
+ 
+       // closing the scanner object
+       scanner.close();
+   }
 }
+
--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -15,6 +15,36 @@ class Timestamps {
         int seconds2 = scanner.nextInt();
 
 
+
+      int hoursBefore = scanner.nextInt();
+      int minutesBefore = scanner.nextInt();
+      int secondsBefore = scanner.nextInt();
+      int hoursAfter = scanner.nextInt();
+      int minutesAfter = scanner.nextInt();
+      int secondsAfter = scanner.nextInt();
+
+      int hoursBefore * 3600;
+      int hoursAfter * 3600;
+      int minutesBefore * 60;
+      int minutesAfter * 60;
+      int secondsAfter * 1;
+      int secondsBefore * 1;
+
+      int resultHours = hoursAfter - hoursBefore;
+      int resultMinutes = minutesAfter - minutesBefore;
+      int resultsSeconds = secondsAfter - secondsBefore;
+
+      int resultFinal = resultHours + resultMinutes + resultsSeconds;
+
+      System.out.println(resultFinal);
+
+
+
+      
+
+    
+
+          
         /*
          *  your code goes here
          */

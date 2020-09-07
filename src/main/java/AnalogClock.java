a9753ba9b2eba8e24edee0c1a8a3f32be865309b
Kyle Gohr <kyle.279965@nv.ccsd.net>
2020-09-07 03:34:40 +0000

    analog clock

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -5,10 +5,19 @@ class AnalogClock {
 
         //Creates a new scanner for user input
         Scanner scanner = new Scanner(System.in);
+      int hourDegree = scanner.nextInt();
+
+
+      int passedHrs = hourDegree / 30;
+      int degPassed = passedHrs * 30;
+      int passedMin = hourDegree - degPassed;
+      int minDegree = (30 - passedMin) * 6;
+      System.out.print(minDegree);
+
 
         //Reads ints from user
 
-
+      
         /*
          *  your code goes here
          */
